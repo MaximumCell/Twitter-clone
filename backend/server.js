5ae404754cb9cbca5503c547f81bf2489c09d69c
MaximumCell <alihassanwinner@gmail.com>
2025-04-10 15:52:23 +0500

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from 'cloudinary';
@@ -22,6 +23,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({limit: "5mb"})); // Middleware to parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded requests
 app.use(cookieParser()); // Middleware to parse cookies
@@ -36,6 +39,13 @@ app.use('/api/auth', authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes); 
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
